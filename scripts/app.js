50585a2fb9ef1ee395e5d57395afc577ff671b30
Cristiana Oliveira <cristiana.oliveira@bullseye.pt>
2016-08-29 12:18:05 +0100

    add data through ng-model and manipulate scopes

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,24 @@
 angular.module("todoListApp", [])
-.controller('mainCtrl', function($scope){
-	$scope.helloworld = function(){
+
+.controller('mainCtrl', function($scope) {
+	$scope.helloWorld = function(){
 		console.log("this is the hello world controller function in the mainCtrl!");
+	};
+})
+
+.controller('coolCtrl', function($scope){
+	$scope.whoAmI = function(){
+		console.log("hello there. this is the coolCtrl function!");
 	}
-}); 
+
+	$scope.helloWorld = function() {
+		console.log("this is not the main ctrl");
+	}
+})
+
+.controller('imASibling', function($scope) {
+	$scope.foobar = 1234; 
+
+	// do other cool stuff
+
+});
